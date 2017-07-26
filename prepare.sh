26fd401eefdaa36a9bf5855d5ba30e6c155a13bb
Ryoh Kawai <kawairyoh@gmail.com>
2017-07-26 23:23:36 +0900

    Add prepare rpmbuild script.

---
+++ prepare.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# setup rpmbuild files
+#
+# set -x
+set -euo pipefail
+
+readonly RPMMACROS_FILE=".rpmmacros"
+readonly HOME_RPMMACROS="${HOME}/${RPMMACROS_FILE}"
+
+echo "Set ${RPMMACROS_FILE} to homedir"
+if [[ -f "${HOME_RPMMACROS}" ]]; then
+  echo "Found ${HOME_RPMMACROS} overwrite? (y/N) "
+  read yesno
+  case "${yesno}" in
+  [yY][eE][sS] | [yY] )
+    : OK
+    ;;
+  * )
+    : No
+    echo "aborted"
+    exit 1
+    ;;
+  esac
+fi
+
+: "Set .rpmmacros file to home directory"
+ln -sf "${PWD}/${RPMMACROS_FILE}" "${HOME_RPMMACROS}"
